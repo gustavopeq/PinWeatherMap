51a30b6267a0365af1171bb2cf54b5a7cfd2aa76
Gustavo Pequeno <gustavo_pequeno2@hotmail.com>
2022-01-01 21:03:06 -0800

    BottomSheet created and MarkerClickListener set

--- app/src/main/java/gustavo/projects/pinweathermap/map/ui/MapsFragment.kt
+++ app/src/main/java/gustavo/projects/pinweathermap/map/ui/MapsFragment.kt
@@ -2,18 +2,25 @@ package gustavo.projects.pinweathermap.map.ui
 
 import androidx.fragment.app.Fragment
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.viewModels
+import androidx.viewbinding.ViewBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import dagger.hilt.android.AndroidEntryPoint
 import gustavo.projects.pinweathermap.R
+import gustavo.projects.pinweathermap.databinding.BottomSheetDetailsBinding
+import gustavo.projects.pinweathermap.databinding.FragmentMapsBinding
 import gustavo.projects.pinweathermap.domain.model.LocationWeather
 import gustavo.projects.pinweathermap.map.viewmodel.MapViewModel
 
@@ -29,12 +36,20 @@ class MapsFragment : Fragment(),
 
     private val viewModel: MapViewModel by viewModels()
 
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<*>
+    private lateinit var bottomSheet: View
+
+    private lateinit var mapBinding: FragmentMapsBinding
+    private lateinit var bottomSheetBinding: BottomSheetDetailsBinding
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+        mapBinding = FragmentMapsBinding.inflate(inflater, container, false)
+
+        return mapBinding.root
     }
 
 
@@ -51,6 +66,11 @@ class MapsFragment : Fragment(),
                 addMarker(locationWeather)
             }
         }
+
+
+        bottomSheetBinding = mapBinding.bottomSheet
+        bottomSheet = bottomSheetBinding.root
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
     }
 
     override fun onMapReady(p0: GoogleMap) {
@@ -59,13 +79,24 @@ class MapsFragment : Fragment(),
         googleMap.setOnMapClickListener(this)
         googleMap.setOnCameraIdleListener(this)
         googleMap.setOnCameraMoveStartedListener(this)
+        googleMap.setOnInfoWindowClickListener {
+            if(it.tag is LocationWeather) {
+                onBottomSheetExpand(it.tag as LocationWeather)
+            }
+        }
     }
 
     private fun addMarker(locationWeather: LocationWeather) {
-        googleMap.addMarker(
-                MarkerOptions()
-                        .position(LatLng(locationWeather.latitude, locationWeather.longitude))
-                        .title("${locationWeather.name} ${locationWeather.temp} °C"))!!.showInfoWindow()
+        if(!locationWeather.name.isNullOrBlank()) {
+            val markerOptions = MarkerOptions()
+                .position(LatLng(locationWeather.latitude, locationWeather.longitude))
+                .title("${locationWeather.name} ${locationWeather.temp} °C")
+
+            googleMap.addMarker(markerOptions)!!.apply {
+                tag = locationWeather
+                showInfoWindow()
+            }
+        }
     }
 
     override fun onMapClick(p0: LatLng) {
@@ -80,5 +111,12 @@ class MapsFragment : Fragment(),
 
     override fun onCameraMoveStarted(p0: Int) {
         googleMap.clear()
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
     }
+
+    private fun onBottomSheetExpand(locationWeather: LocationWeather) {
+            bottomSheetBinding.cityName.text = locationWeather.name
+            bottomSheetBinding.temperature.text = locationWeather.temp.toString()
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
 }
